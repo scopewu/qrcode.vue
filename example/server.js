cdb60ebda34a8dd4fd00dbaff428af78beb09fe8
scopewu <scopewu@gmail.com>
2022-07-06 16:58:02 +0800

    chore: upgrade some dependencies.

--- example/server.js
+++ example/server.js
@@ -1,17 +1,30 @@
 const http = require('http')
 const { createSSRApp } = require('vue')
-const { renderToString } = require('@vue/server-renderer')
+const { renderToString } = require('vue/server-renderer')
 const QrcodeVue = require('../dist/qrcode.vue.cjs')
 
 const PORT = process.env.PORT || 3000
 
 const app = createSSRApp({
+  data: () => ({ value: 'Hello QrcodeVue', size: 100 }),
   components: { QrcodeVue },
-  template: '<QrcodeVue value="123" :size="100" render-as="svg" />',
+  template: '<QrcodeVue :value="value" :size="size" render-as="svg" />',
 });
 
 http.createServer(async (request, response) => {
-  const html = await renderToString(app)
+  const _html = await renderToString(app)
+
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Qrcode.vue SSR Example</title>
+      </head>
+      <body>
+        <div>${_html}</div>
+      </body>
+    </html>
+    `
 
   response.writeHead(200, {
     'Content-Type': 'text/html',
