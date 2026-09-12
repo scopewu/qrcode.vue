e2a9a27d8e09fcbc088015eec25caa9756872c21
scopewu <scopewu@gmail.com>
2026-09-12 17:00:22 +0800

    chore(eslint): replace deprecated tseslint.config with defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: [
       'dist/',
